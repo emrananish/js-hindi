758277ed168f393007f1782ae80d69adc1799a56
emrananish <emrananish@gmail.com>
2024-08-21 15:11:52 +0530

    Datatype conversion confusion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,14 +1,26 @@
 //let score = "33";
 //let score = "Emran";
 //let score = null
-let score = true
 
-console.log(typeof score);
-console.log(typeof(score));
+// let score = true
 
-let valueInNumber = Number(score)
+// console.log(typeof score);
+// console.log(typeof(score));
 
+// let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "sdsds"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn); 
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
