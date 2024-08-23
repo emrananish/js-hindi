cc9153aa59adfcc60e8abb786e260e154341d2f7
emrananish <emrananish@gmail.com>
2024-08-23 16:04:20 +0530

    Data types of javascript summary

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -2,12 +2,12 @@
 //let score = "Emran";
 //let score = null
 
-// let score = true
+let score = true
 
 // console.log(typeof score);
 // console.log(typeof(score));
 
-// let valueInNumber = Number(score)
+let valueInNumber = Number(score)
 
 // console.log(typeof valueInNumber);
 // console.log(valueInNumber);
@@ -19,8 +19,66 @@
 let isLoggedIn = "sdsds"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn); 
+// console.log(booleanIsLoggedIn); 
 
 // 1 => true; 0 => false
 // "" => false
 // "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello";
+let str2 = " World";
+let str3 = str1 + str2
+//console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+//console.log(gameCounter);
+
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
